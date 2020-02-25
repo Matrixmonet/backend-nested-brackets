9b48e2e8edb047c9bc22c4f3de56720ac3b6c089
Matrixmonet <successismyne@gmail.com>
2020-02-25 14:16:37 -0500

    Initial Commit

--- nested.py
+++ nested.py
@@ -1,23 +1,45 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Module docstring: One line description of what your program does.
-"""
-__author__ = "???"
+# python3
 
 import sys
 
 
-def is_nested(line):
-    """Validate a single input line for correct nesting"""
-    pass
+class Bracket:
+
+    def __init__(self, bracket_type, position):
+        self.bracket_type = bracket_type
+        self.position = position
+
+    def match(self, char):
+        if self.bracket_type == '[' and char == ']':
+            return True
+        if self.bracket_type == '{' and char == '}':
+            return True
+        if self.bracket_type == '(' and char == ')':
+            return True
+        return False
 
 
-def main(args):
-    """Open the input file and call `is_nested()` for each line"""
-    # Results: print to console and also write to output file
-    pass
+def checker(text):
+    stack = []
+    for index, char in enumerate(text, start=1):
+
+        if char in ("[", "(", "{"):
+            stack.append(Bracket(char, index))
+
+        elif char in ("]", ")", "}"):
+            if not stack:
+                return index
+
+            top = stack.pop()
+            if not top.match(char):
+                return index
+    if stack:
+        top = stack.pop()
+        return top.position
+
+    return "Success"
 
 
-if __name__ == '__main__':
-    main(sys.argv[1:])
+if __name__ == "__main__":
+    text = sys.stdin.read().strip("\n")
+    print(checker(text))
